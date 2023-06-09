3e002f6826c6bbdfc94e2e6022cda8f2b10b2948
MightyActionX <99732969+KKKabutack@users.noreply.github.com>
2023-06-09 14:43:25 +0800

    Add files via upload

---
+++ Assignment3_Wang_Huidong/src/a3e/Main.java
@@ -0,0 +1,14 @@
+package a3e;
+
+
+public class Main {
+    /**
+     * Main function.
+     * Only to initiate GUI.
+     * @auther Wang Huidong
+     */
+    public static void main(String[] args) {
+        MainGUI gui = new MainGUI();
+        gui.go();
+    }
+}
